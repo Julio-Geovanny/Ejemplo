10dab9c2c03f69335e3a90bc39b5d24faa0ccb63
Julio-Geovanny <geovannychg14@gmail.com>
2024-10-05 20:20:57 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejemplo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+//C: \Users\Geovanny\source\repos
