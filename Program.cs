629488fcf685f2323deee41df0e8c72d9c944545
Julio-Geovanny <166413723+Julio-Geovanny@users.noreply.github.com>
2024-10-05 20:56:10 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejemplo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
+
+//C: \Users\Geovanny\source\repos
